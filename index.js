b14b4551660ab140dee4d00cc7b3f2c84aaa9d3e
Randy Herasme <flatironschool@Flatirons-MacBook-Air.local>
2018-11-19 13:06:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution here:
+function driversWithRevenueOver(drivers, revenue){
+  return drivers.filter(function(driver){
+    return driver.revenue > revenue
+  })
+}
+
+function driverNamesWithRevenueOver(drivers, revenue){
+  new_arr = driversWithRevenueOver(drivers, revenue)
+  return new_arr.map(function(driver){
+    return driver.name
+  })
+}
+
+function exactMatch(drivers, obj){
+
+  if (Object.keys(obj)[0] == 'name'){
+  return drivers.filter(function(driver){
+    return driver.name == obj.name
+  })
+  }
+
+  if (Object.keys(obj)[0] == 'revenue'){
+  return drivers.filter(function(driver){
+    return driver.revenue == obj.revenue
+  })
+  }
+
+}
+
+function exactMatchToList(drivers, revenue){
+  new_arr = exactMatch(drivers, revenue)
+  return new_arr.map(function(driver){
+    return driver.name
+  })
+}
